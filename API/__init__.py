d9a1563e3615deb3cd78bec18dd5d6aa7afc6f53
Pratik-Sys <pithore.pratik@gmail.com>
2021-07-30 17:49:48 +0530

    import all the modules and it's config

--- API/__init__.py
+++ API/__init__.py
@@ -1,1 +1,17 @@
+from flask import Flask
+from flask_restx import Resource, Api
+from flask_mongoengine import MongoEngine
+from dotenv import load_dotenv
+import os
+import random, string
 
+
+app = Flask(__name__)
+api = Api(app)
+load_dotenv(".env")
+app.config["MONGODB_SETTINGS"] = {"host": os.getenv("URI")}
+db = MongoEngine(app)
+app.config["SECRET_KEY"] = "".join(
+    random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits)
+    for _ in range(16)
+)
