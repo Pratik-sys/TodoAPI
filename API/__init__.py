dd2670fa370c62cd4e7b21a7bfb037cfa7ae3ebd
Pratik-Sys <pithore.pratik@gmail.com>
2021-08-18 11:11:30 +0530

    setup JWT extended

--- API/__init__.py
+++ API/__init__.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 import os
 import random, string
 from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager
 
 
 app = Flask(__name__)
@@ -16,6 +17,14 @@ app.config["SECRET_KEY"] = "".join(
     random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits)
     for _ in range(16)
 )
+
 bcrypt = Bcrypt(app)
+
+app.config['JWT_SECRET_KEY'] = "".join(
+    random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits)
+    for _ in range(16)
+) 
+
+jwt = JWTManager(app)
 
 from API import routes
