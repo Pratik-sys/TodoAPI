4834ec44ab8b51b217b48f2ca16ac768f8c0a2a4
Pratik-Sys <pithore.pratik@gmail.com>
2021-08-18 11:03:15 +0530

    Revert "Implement login manager"
    This reverts commit 4f9d0339ad02ed4214561b678ab767376a60a48e.

--- API/__init__.py
+++ API/__init__.py
@@ -1,12 +1,10 @@
-import os
 from flask import Flask
 from flask_restx import Api
 from flask_mongoengine import MongoEngine
 from dotenv import load_dotenv
-import random
-import string
+import os
+import random, string
 from flask_bcrypt import Bcrypt
-from flask_login import LoginManager
 
 
 app = Flask(__name__)
@@ -15,11 +13,9 @@ load_dotenv(".env")
 app.config["MONGODB_SETTINGS"] = {"host": os.getenv("URI")}
 db = MongoEngine(app)
 app.config["SECRET_KEY"] = "".join(
-    random.choice(string.ascii_uppercase +
-                  string.ascii_lowercase + string.digits)
+    random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits)
     for _ in range(16)
 )
 bcrypt = Bcrypt(app)
-login_manager = LoginManager(app)
 
 from API import routes
--- API/models.py
+++ API/models.py
@@ -1,13 +1,7 @@
-from API import db, login_manager
-from flask_login import UserMixin
+from API import db
 
 
-@login_manager.user_loader
-def load_user(user_id):
-    return User.objects(id=user_id).first()
-
-
-class User(UserMixin, db.Document):
+class User(db.Document):
     name = db.StringField()
     nickname = db.StringField()
     email = db.StringField()
@@ -15,15 +9,15 @@ class User(UserMixin, db.Document):
     date = db.DateTimeField()
 
 
-class Todo(UserMixin, db.Document):
+class Todo(db.Document):
     user = db.ReferenceField(User)
     title = db.StringField()
     theme = db.StringField()
     date = db.DateTimeField()
 
 
-class Subtask(UserMixin, db.Document):
+class Subtask(db.Document):
     todo = db.ReferenceField(Todo, reverse_delete_rule=db.CASCADE)
     taskName = db.StringField()
-    completed = db.BooleanField(default=False)
+    completed = db.BooleanField(default = False)
     date = db.DateTimeField()
