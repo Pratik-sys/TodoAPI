e6369151faf55d992f108b2340d9b54820cad7a0
Pratik-Sys <pithore.pratik@gmail.com>
2021-08-19 22:26:07 +0530

    fix few things in mongo validation

--- API/models.py
+++ API/models.py
@@ -10,19 +10,19 @@ class User(db.Document):
     name = db.StringField(required=True)
     nickname = db.StringField(required=True,min_length=3, max_length=10)
     email = db.StringField(required=True, unique=True)
-    password = db.StringField(required=True, min_length=8,max_length=18)
-    date = db.DateTimeField(default=datetime.utcnow().strftime("%H:%M:%S %b %d %Y"))
+    password = db.StringField(required=True)
+    date = db.DateTimeField(default=datetime.utcnow)
 
 
 class Todo(db.Document):
     user = db.ReferenceField(User)
     title = db.StringField()
     theme = db.StringField()
-    date = db.DateTimeField(default=datetime.utcnow().strftime("%H:%M:%S %b %d %Y"))
+    date = db.DateTimeField(default=datetime.utcnow)
 
 
 class Subtask(db.Document):
     todo = db.ReferenceField(Todo, reverse_delete_rule=db.CASCADE)
     taskName = db.StringField()
     completed = db.BooleanField(default = False)
-    date = db.DateTimeField(default=datetime.utcnow().strftime("%H:%M:%S %b %d %Y"))
+    date = db.DateTimeField(default=datetime.utcnow)
