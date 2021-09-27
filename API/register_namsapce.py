dc8de088f90cd4589777e29a48f406529653bdad
Pratik-Sys <pithore.pratik@gmail.com>
2021-09-27 18:27:43 +0530

    disable swagger

--- API/register_namsapce.py
+++ API/register_namsapce.py
@@ -5,7 +5,7 @@ from .Todos.routes import todos
 from .Users.routes import users
 
 blueprint = Blueprint("api", __name__)
-api = Api(blueprint, title="Todo Api", version="1.0")
+api = Api(blueprint, title="Todo Api", version="1.0", doc=False)
 
 api.add_namespace(subtasks)
 api.add_namespace(todos)
